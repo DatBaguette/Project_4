a00a4eb2ebcca99152fb733535b5425dd34c32ce
crakicuri <38381840+crakicuri@users.noreply.github.com>
2020-04-02 15:46:51 +0200

    @crakicuri

--- Assets/GameManager/Script_C#/GameManager.cs
+++ Assets/GameManager/Script_C#/GameManager.cs
@@ -4,15 +4,17 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public Vector3 RetrievePosition()
     {
-        
-    }
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+        RaycastHit hit;
+
+        if (Physics.Raycast(ray, out hit, 100))
+        {
+            return (hit.point);
+        }
+
+        return (hit.point);
     }
 }
