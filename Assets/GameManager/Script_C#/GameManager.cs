547799d88ffcdcb0090e4250b3fe77b52a48eeff
crakicuri <38381840+crakicuri@users.noreply.github.com>
2020-04-03 02:58:33 +0200

    @crakicuri

--- Assets/GameManager/Script_C#/GameManager.cs
+++ Assets/GameManager/Script_C#/GameManager.cs
@@ -2,17 +2,19 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class GameManager : MonoBehaviour
+public class GameManager : Singleton<GameManager>
 {
-    // Start is called before the first frame update
-    void Start()
+    public Vector3 RetrievePosition()
     {
-        
-    }
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+        RaycastHit hit;
+
+        if (Physics.Raycast(ray, out hit, 100))
+        {
+            return (hit.point);
+        }
+
+        return (hit.point);
     }
 }
---
+++ Assets/GameManager/Script_C#/Singleton.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Singleton<T> : MonoBehaviour where T : MonoBehaviour
+{
+
+    private static T _instance;
+
+    public static T Instance
+    {
+
+        get
+        {
+
+            if (_instance == null)
+            {
+
+                _instance = (T)FindObjectOfType(typeof(T));
+
+                if (_instance == null)
+                {
+
+                    _instance = new GameObject("GameController").AddComponent<T>();
+                }
+            }
+            return _instance;
+        }
+    }
+}
