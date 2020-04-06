1e039ff942cdb5385405029f70a66e068d5df3b5
DatBaguette <62941149+DatBaguette@users.noreply.github.com>
2020-04-06 18:49:07 +0200

    @datbaguette

--- Assets/GameManager/Script_C#/GameManager.cs
+++ Assets/GameManager/Script_C#/GameManager.cs
@@ -2,8 +2,29 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class GameManager : Singleton<GameManager>
 {
+    public enum m_PlayerState
+    {
+        move_player,
+        boomerang,
+        mouve_drone1,
+        mouve_drone2,
+        mouve_drone3,
+        cinématique,
+        Idle,
+        Menu
+    }
+
+    public m_PlayerState m_currentPlayerState;
+
+
+    private void Start()
+    {
+        m_currentPlayerState = m_PlayerState.move_player;
+    }
+
     public Vector3 RetrievePosition()
     {
         Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
@@ -16,5 +37,33 @@ public class GameManager : Singleton<GameManager>
         }
 
         return (hit.point);
+    }
+
+
+
+    private void Update()
+    {
+        StateController();
+    }
+
+
+    public void StateController()
+    {
+        if (Input.GetKeyDown("z"))
+        {
+            m_currentPlayerState = m_PlayerState.boomerang;
+        }
+        if (Input.GetKeyDown("a"))
+        {
+            m_currentPlayerState = m_PlayerState.move_player;
+        }
+        if (Input.GetKeyDown("e"))
+        {
+            m_currentPlayerState = m_PlayerState.mouve_drone1;
+        }
+        if (Input.GetKeyDown("e"))
+        {
+            m_currentPlayerState = m_PlayerState.mouve_drone2;
+        }
     }
 }
