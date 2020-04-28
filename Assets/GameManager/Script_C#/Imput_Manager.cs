62ad12712f1d888c0ff55cf46c4622b02dc39544
DatBaguette <62941149+DatBaguette@users.noreply.github.com>
2020-04-28 16:52:00 +0200

    @datbaguette
    finish shader

---
+++ Assets/GameManager/Script_C#/Imput_Manager.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Imput_Manager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public bool GetInput()
+    {
+        bool is_touched = true;
+
+
+        return (is_touched);
+    }
+}
