cf9db3de6a9d07781f20a11f452243b2b23f3dcb
DatBaguette <62941149+DatBaguette@users.noreply.github.com>
2020-04-28 16:45:21 +0200

    @datbaguette
    yes

---
+++ Assets/GameManager/Script_C#/Imput_Manager.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Imput_Manager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public bool GetInput()
+    {
+
+
+
+        return ();
+    }
+}
