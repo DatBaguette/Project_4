d094faf51594ce2d0e7016727e833f505c378338
crakicuri <38381840+crakicuri@users.noreply.github.com>
2020-04-29 20:30:58 +0200

    @crakicuri

---
+++ Assets/World/Script/C#_Script/Object/DestructiblesObjects.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestructiblesObjects : MonoBehaviour, IFireReact
+{
+
+    void IFireReact.OnFire()
+    {
+        Debug.Log("ça brule !!!!!!!!!!!!!!!");
+
+        Destroy(gameObject);
+    }
+
+    void IFireReact.OnKillFire()
+    {
+        Debug.Log("Boom");
+    }
+
+    
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
