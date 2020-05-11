9b3918cb09cacdc83c590f3f171efe722a78e317
DatBaguette <62941149+DatBaguette@users.noreply.github.com>
2020-05-11 14:46:29 +0200

    @datbaguette

--- Assets/Player/Script/C#_Script/Manage_Robot.cs
+++ Assets/Player/Script/C#_Script/Manage_Robot.cs
@@ -64,6 +64,15 @@ public class Manage_Robot : Singleton<Manage_Robot>
     [SerializeField]
     public int[] price;
 
+    public bool CanYouCraft = true;
+    public Transform NextNodeToCreatARobot;
+    private RobotCraftHelper CraftHelper;
+
+    private void Start()
+    {
+        CraftHelper = m_player.GetComponentInChildren<RobotCraftHelper>();
+
+    }
     public void ChooseRobot(int robotType)
     {
         m_actualCraftRobot = robotType;
@@ -137,9 +146,9 @@ public class Manage_Robot : Singleton<Manage_Robot>
 
                     switch (CraftManager.Instance.m_choosenSize)
                     {
-                        case 1: InstantiateRobot(m_FlyingBot_1); break;
+                        case 1: InstantiateRobot(m_FlyingBot_1, NextNodeToCreatARobot); break;
 
-                        case 2: InstantiateRobot(m_FlyingBot_2); break;
+                        case 2: InstantiateRobot(m_FlyingBot_2, NextNodeToCreatARobot); break;
                     }
 
                     break;
@@ -152,32 +161,26 @@ public class Manage_Robot : Singleton<Manage_Robot>
 
                         case 2: GameManager.Instance.m_actualRessources.Value -= price[3]; break;
                     }
-
                     switch (CraftManager.Instance.m_choosenSize)
                     {
-                        case 1: InstantiateRobot(m_PlatformeBot_1); break;
+                        case 1: InstantiateRobot(m_PlatformeBot_1, NextNodeToCreatARobot); break;
 
-                        case 2: InstantiateRobot(m_PlatformeBot_2); break;
+                        case 2: InstantiateRobot(m_PlatformeBot_2, NextNodeToCreatARobot); break;
                     }
-
                     break;
-
                 case 2:
-
                     switch (m_craftManager.m_choosenSize)
                     {
                         case 1: GameManager.Instance.m_actualRessources.Value -= price[4]; break;
 
                         case 2: GameManager.Instance.m_actualRessources.Value -= price[5]; break;
                     }
-
                     switch (CraftManager.Instance.m_choosenSize)
                     {
-                        case 1: InstantiateRobot(m_DestructionBot_1); break;
+                        case 1: InstantiateRobot(m_DestructionBot_1, NextNodeToCreatARobot); break;
 
-                        case 2: InstantiateRobot(m_DestructionBot_2); break;
+                        case 2: InstantiateRobot(m_DestructionBot_2, NextNodeToCreatARobot); break;
                     }
-
                     break;
             }
         }
@@ -187,100 +190,65 @@ public class Manage_Robot : Singleton<Manage_Robot>
     
     //Instantiate Robot and give him properties to work
 
-    public void InstantiateRobot(GameObject p_prebabbot)
+    public void InstantiateRobot(GameObject p_prebabbot , Transform p_Node)
     {
-
-        GameManager.Instance.m_robotNumber += 1;
-
-        // Robot Object
-        var robot = Instantiate(p_prebabbot, m_player.gameObject.transform);
-        robot.transform.SetParent(m_robotContainer.transform, false);
-        robot.gameObject.name = "robot " + GameManager.Instance.m_robotNumber;
-
-        robot.transform.position = m_player.gameObject.transform.position + new Vector3(2, 0, 0);
-
-        // Add the robot to a global list
-        if (GameManager.Instance.m_robots.Count >= GameManager.Instance.m_robotNumber)
+        if(GameManager.Instance.m_robotNumber < 4)
         {
-            GameManager.Instance.m_robots[GameManager.Instance.m_robotNumber-1] = robot;
+
+            GameManager.Instance.m_robotNumber += 1;
+            CraftHelper.NextNode();
+
+
+            // Robot Object
+            var robot = Instantiate(p_prebabbot, m_player.gameObject.transform);
+            robot.transform.SetParent(m_robotContainer.transform, false);
+            robot.gameObject.name = "robot " + GameManager.Instance.m_robotNumber;
+
+            //robot.transform.position = m_player.gameObject.transform.position + p_Node.position ;
+            robot.transform.position = p_Node.position;
+
+            // Add the robot to a global list
+            if (GameManager.Instance.m_robots.Count >= GameManager.Instance.m_robotNumber)
+            {
+                GameManager.Instance.m_robots[GameManager.Instance.m_robotNumber - 1] = robot;
+            }
+            else
+                GameManager.Instance.m_robots.Add(robot);
+
+            // Robot Infos
+            RobotInisialisation m_robotInfosScript = robot.GetComponent<RobotInisialisation>();
+            m_robotInfosScript.m_size = Mathf.FloorToInt(CraftManager.Instance.m_choosenSize);
+
+            // Robot Values
+            RobotMovement robotScriptToMove = robot.gameObject.GetComponent<RobotMovement>();
+            robotScriptToMove.m_thisEntityNumber = GameManager.Instance.m_robotNumber;
+
+            // Robot UI
+            var robotUi = Instantiate(m_robotUIPrefab);
+            robotUi.transform.SetParent(m_robotUIContainer.transform, false);
+
+            // Add the robot UI to a global list
+            if (GameManager.Instance.m_robotsUI.Count >= GameManager.Instance.m_robotNumber)
+                GameManager.Instance.m_robotsUI[GameManager.Instance.m_robotNumber - 1] = robotUi;
+            else
+                GameManager.Instance.m_robotsUI.Add(robotUi);
+
+            ChooseRobot chooseRobotScript = robotUi.gameObject.GetComponent<ChooseRobot>();
+            chooseRobotScript.m_uiRobotNumber = GameManager.Instance.m_robotNumber;
+            chooseRobotScript.m_associateRobot = robot;
+            chooseRobotScript.m_player = m_player;
+
+            Button robotUiButton = robotUi.gameObject.GetComponent<Button>();
+            robotUiButton.onClick.AddListener(chooseRobotScript.SelectRobot);
+
+            var UIName = robotUi.gameObject.transform.Find("Text");
+
+            Text UINameText = UIName.gameObject.GetComponent<Text>();
+            UINameText.text = robot.gameObject.name;
         }
-        else
-            GameManager.Instance.m_robots.Add(robot);
-
-        // Robot Infos
-        RobotInisialisation m_robotInfosScript = robot.GetComponent<RobotInisialisation>();
-        m_robotInfosScript.m_size = Mathf.FloorToInt(CraftManager.Instance.m_choosenSize);
-
-        // Robot Values
-        RobotMovement robotScriptToMove = robot.gameObject.GetComponent<RobotMovement>();
-        robotScriptToMove.m_thisEntityNumber = GameManager.Instance.m_robotNumber;
-
-        // Robot UI
-        var robotUi = Instantiate(m_robotUIPrefab);
-        robotUi.transform.SetParent(m_robotUIContainer.transform, false);
-
-        // Add the robot UI to a global list
-        if (GameManager.Instance.m_robotsUI.Count >= GameManager.Instance.m_robotNumber)
-            GameManager.Instance.m_robotsUI[GameManager.Instance.m_robotNumber-1] = robotUi;
-        else
-            GameManager.Instance.m_robotsUI.Add(robotUi);
-
-        ChooseRobot chooseRobotScript = robotUi.gameObject.GetComponent<ChooseRobot>();
-        chooseRobotScript.m_uiRobotNumber = GameManager.Instance.m_robotNumber;
-        chooseRobotScript.m_associateRobot = robot;
-        chooseRobotScript.m_player = m_player;
-
-        Button robotUiButton = robotUi.gameObject.GetComponent<Button>();
-        robotUiButton.onClick.AddListener(chooseRobotScript.SelectRobot);
-
-        var UIName = robotUi.gameObject.transform.Find("Text");
-
-        Text UINameText = UIName.gameObject.GetComponent<Text>();
-        UINameText.text = robot.gameObject.name;
     }
 
-    //public void InstantiateRobot(RobotInisialisation RobotCreat)
-    //{
-    //    GameManager.Instance.m_robotNumber += 1;
-
-    //    // Robot Object
-    //    var robot = Instantiate(m_robotPrefab, m_player.gameObject.transform);
-    //    robot.transform.SetParent(m_robotContainer.transform, false);
-    //    robot.gameObject.name = "robot " + GameManager.Instance.m_robotNumber;
-    //    robot.gameObject.transform.localScale *= m_craftManager.m_choosenSize;
-    //    robot.gameObject.transform.localScale /= 1.5f;
-
-    //    // Robot Values
-    //    ClickToMoveEntity robotScriptToMove = robot.gameObject.GetComponent<ClickToMoveEntity>();
-    //    robotScriptToMove.m_thisEntityNumber = GameManager.Instance.m_robotNumber;
-
-    //    // Assign robot type
-    //    RobotInisialisation robotTypeScript = robot.gameObject.GetComponent<RobotInisialisation>();
-    //    robotTypeScript.m_robotTypeInCreation = actualRobotType;
-    //    robotTypeScript.m_robotSize = m_craftManager.m_choosenSize;
-    //    robotTypeScript.SelectRobotType();
-    //    robotTypeScript.AssignRobotType();
-
-    //    // Robot UI
-    //    var robotUi = Instantiate(m_robotUIPrefab);
-    //    robotUi.transform.SetParent(m_robotUIContainer.transform, false);
-
-    //    ChooseRobot chooseRobotScript = robotUi.gameObject.GetComponent<ChooseRobot>();
-    //    chooseRobotScript.m_uiRobotNumber = GameManager.Instance.m_robotNumber;
-    //    chooseRobotScript.m_associateRobot = robot;
-    //    chooseRobotScript.m_player = m_player;
-
-    //    Button robotUiButton = robotUi.gameObject.GetComponent<Button>();
-    //    robotUiButton.onClick.AddListener(chooseRobotScript.SelectRobot);
-
-    //    var UIName = robotUi.gameObject.transform.Find("Text");
-
-    //    Text UINameText = UIName.gameObject.GetComponent<Text>();
-    //    UINameText.text = robot.gameObject.name;
-
-    //    // Change position ( dont worl ) :(
-    //    robot.gameObject.transform.position += new Vector3(127, 7, 87);
-    //}
+    
 
     // Check if the player have enough money in the GameManager
     private bool CheckIfEnoughMoney(int price)
