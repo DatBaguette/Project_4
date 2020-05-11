b61900fffe1867ac9319d1cdcb6b36e2f38abdd3
DatBaguette <62941149+DatBaguette@users.noreply.github.com>
2020-05-11 17:22:12 +0200

    @datbaguette

---
+++ Assets/GD2Lib/Scripts/FloatVar.cs
@@ -0,0 +1,12 @@
+﻿
+using UnityEngine;
+
+namespace GD2Lib
+{
+    /// <summary>
+    /// <see cref="GenericVar{T}"/> de type float.
+    /// </summary>
+    [CreateAssetMenu(fileName = "GD2-FloatVar", menuName = "GD2Lib/Variables/FloatVar")]
+    public class FloatVar : GenericVar<float> { }
+
+}
---
+++ Assets/GD2Lib/Scripts/Vector3Var.cs
@@ -0,0 +1,13 @@
+﻿
+using UnityEngine;
+
+namespace GD2Lib
+{
+
+    /// <summary>
+    /// <see cref="GenericVar{T}"/> de type Vector3.
+    /// </summary>
+    [CreateAssetMenu(fileName = "GD2-Vector3Var", menuName = "GD2Lib/Variables/Vector3Var")]
+    public class Vector3Var : GenericVar<Vector3> { }
+
+}
