c8e102f39532e2d13dfb54dd7957be3395283cc6
crakicuri <38381840+crakicuri@users.noreply.github.com>
2020-05-13 13:36:19 +0200

    @crakicuri

--- Assets/Ui/Script/C#_Script/MenuManager.cs
+++ Assets/Ui/Script/C#_Script/MenuManager.cs
@@ -145,4 +145,9 @@ public class MenuManager : Singleton<MenuManager>
     {
         SceneManager.LoadScene("Main_Menu");
     }
+
+    public void OpenBrowserForFeedbacks()
+    {
+        Application.OpenURL("https://docs.google.com/forms/d/e/1FAIpQLSeACeRAw54Zd0whN3faM3H6IcdtmiYor_04sxPtPw867ZVm3A/viewform?usp=sf_link");
+    }
 }
