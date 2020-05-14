a92fa36fd4dbf279613b39796bc2e0a357071469
crakicuri <38381840+crakicuri@users.noreply.github.com>
2020-05-14 17:13:42 +0200

    @crakicuri

--- Assets/World/Assets/Prefabs/BOSS/BossBehaviour.cs
+++ Assets/World/Assets/Prefabs/BOSS/BossBehaviour.cs
@@ -142,5 +142,10 @@ public class BossBehaviour : MonoBehaviour, IFireReact
             Destroy(collision.gameObject);
             
         }
+
+        if ( collision.gameObject.GetComponent<ClickToMoveEntity>())
+        {
+            GameManager.Instance.playerDeath();
+        }
     }
 }
