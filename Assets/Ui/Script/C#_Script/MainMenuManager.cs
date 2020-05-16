1287b2d542edccce226eb43cf3275b724069a788
crakicuri <38381840+crakicuri@users.noreply.github.com>
2020-05-16 15:01:36 +0200

    @crakicuri

--- Assets/Ui/Script/C#_Script/MainMenuManager.cs
+++ Assets/Ui/Script/C#_Script/MainMenuManager.cs
@@ -20,6 +20,6 @@ public class MainMenuManager : MonoBehaviour
 
     public void OpenBrowserForFeedbacks()
     {
-        Application.OpenURL("https://docs.google.com/forms/d/e/1FAIpQLSeACeRAw54Zd0whN3faM3H6IcdtmiYor_04sxPtPw867ZVm3A/viewform?usp=sf_link");
+        Application.OpenURL("https://docs.google.com/forms/d/e/1FAIpQLSfExEdubQvAVt_tSGWCivZEKC8owTmXVLdGz6VoBYyUstQggQ/viewform?usp=sf_link");
     }
 }
--- Assets/Ui/Script/C#_Script/MenuManager.cs
+++ Assets/Ui/Script/C#_Script/MenuManager.cs
@@ -170,6 +170,6 @@ public class MenuManager : Singleton<MenuManager>
 
     public void OpenBrowserForFeedbacks()
     {
-        Application.OpenURL("https://docs.google.com/forms/d/e/1FAIpQLSeACeRAw54Zd0whN3faM3H6IcdtmiYor_04sxPtPw867ZVm3A/viewform?usp=sf_link");
+        Application.OpenURL("https://docs.google.com/forms/d/e/1FAIpQLSfExEdubQvAVt_tSGWCivZEKC8owTmXVLdGz6VoBYyUstQggQ/viewform?usp=sf_link");
     }
 }
